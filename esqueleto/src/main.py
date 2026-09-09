d99d6b157720c0283ef6f552dfd0d185e74570db
Natal1avyy <165675598+Natal1avyy@users.noreply.github.com>
2026-09-09 16:31:58 -0300

    Update main.py

--- esqueleto/src/main.py
+++ esqueleto/src/main.py
@@ -1,4 +1,5 @@
 from src.config import TEMA
+from src.dominio.Recetario import Mostrar_catalogo
 
 TEMAS = {
     "pokedex": "Pokédex",
@@ -38,6 +39,8 @@ def main():
         opcion = input("> ").strip()
         if opcion == "0":
             print("Chau.")
+        elif opcion == "1"
+            Mostrar_catalogo()
         elif opcion in {"1", "2", "3", "4", "5", "6", "7", "8", "9"}:
             pendiente()
         else:
