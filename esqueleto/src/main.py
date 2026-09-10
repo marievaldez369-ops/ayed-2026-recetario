0a85b77ada77dc2fab6a18ea46df871ecae086bb
wendypatinopinto <wendiipatinopinto@gmail.com>
2026-09-10 19:38:19 -0300

    Update main.py

--- esqueleto/src/main.py
+++ esqueleto/src/main.py
@@ -39,7 +39,7 @@ def main():
         opcion = input("> ").strip()
         if opcion == "0":
             print("Chau.")
-        elif opcion == "1"
+        elif opcion == "1":
             Mostrar_catalogo()
         elif opcion in {"1", "2", "3", "4", "5", "6", "7", "8", "9"}:
             pendiente()
