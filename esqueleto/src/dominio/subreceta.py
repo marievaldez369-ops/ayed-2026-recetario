1a1fa64a25ff5435dc752d5038b3815ae682efa7
Natal1avyy <165675598+Natal1avyy@users.noreply.github.com>
2026-09-19 20:25:32 -0300

    Add Subrecetas class and ingredient retrieval function
    Fix issues in Subrecetas class and implement ingredient retrieval.

--- esqueleto/src/dominio/subreceta.py
+++ esqueleto/src/dominio/subreceta.py
@@ -1,1 +1,21 @@
+class Subrecetas: 
+  def __init__ (self, id_receta, nombre, ingredientes=None, subrecetas=None):
+    self.id = id_receta
+    self.nombre = nombre
+    self.ingrediente = ingrediente or []
+    sel.subreceta = subreceta or []
+
+  def resumen(self):
+    return f"{self.id} - {self.nombre}" 
+
+def obtener_ingredientes_totales(receta, recetario):
+  ingredientes = list.(receta.ingredientes)
+
+  for subrecetas in receta.subrecetas:
+    ingredientes.extend (
+      obtener_ingredientes_totales(subreceta, recetario)
+    )
+
+return ingredientes
+
 
