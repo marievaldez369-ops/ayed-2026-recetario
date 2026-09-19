03db3ea79ff834a8460727a9275e79efa7a605c6
Natal1avyy <165675598+Natal1avyy@users.noreply.github.com>
2026-09-19 20:30:02 -0300

    Update receta.py

--- esqueleto/src/dominio/receta.py
+++ esqueleto/src/dominio/receta.py
@@ -1,1 +1,10 @@
+class Receta: 
+  def __init__(self, id_receta, nombre, ingrediente=None, subreceta=None)
+  self.id = = id_receta
+        self.nombre = nombre
+        self.ingredientes = ingredientes or []
+        self.subrecetas = subrecetas or []
+
+    def resumen(self):
+        return f"{self.id} - {self.nombre}"
 
