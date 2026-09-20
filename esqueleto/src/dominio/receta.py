3a636df25d91af5bb9a7d2072115430cfef98a69
wendypatinopinto <wendiipatinopinto@gmail.com>
2026-09-20 14:38:40 -0300

    changes in subreceta.py and receta.py
    update subreceta.py and receta.py

--- esqueleto/src/dominio/receta.py
+++ esqueleto/src/dominio/receta.py
@@ -1,6 +1,6 @@
-class Receta: 
-  def __init__(self, id_receta, nombre, ingrediente=None, subreceta=None)
-  self.id = = id_receta
+class Receta:
+    def __init__(self, id_receta, nombre, ingredientes=None, subrecetas=None):
+        self.id = id_receta
         self.nombre = nombre
         self.ingredientes = ingredientes or []
         self.subrecetas = subrecetas or []
--- esqueleto/src/dominio/subreceta.py
+++ esqueleto/src/dominio/subreceta.py
@@ -1,15 +1,15 @@
-class Subrecetas: 
+class Subreceta:
   def __init__ (self, id_receta, nombre, ingredientes=None, subrecetas=None):
     self.id = id_receta
     self.nombre = nombre
-    self.ingrediente = ingrediente or []
-    sel.subreceta = subreceta or []
+    self.ingredientes = ingredientes or []
+    self.subrecetas = subrecetas or []
 
   def resumen(self):
     return f"{self.id} - {self.nombre}" 
 
-def obtener_ingredientes_totales(receta, recetario):
-  ingredientes = list.(receta.ingredientes)
+def obtener_ingredientes_totales(receta, recetario=None):
+  ingredientes = list(receta.ingredientes)
 
   for subrecetas in receta.subrecetas:
     ingredientes.extend (
